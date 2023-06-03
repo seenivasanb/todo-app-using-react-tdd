fa8f3ed82215af550a129f1497f9d1b4e348ed68
seenivasanb <connecttoseeni@gmail.com>
2023-06-03 17:14:41 +0530

    Add Todo page is implemented

---
+++ src/pages/add-todo-page/add-todo-page.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const AddTodoPage = () => {
+  return (
+    <div>
+        <h1>AddTodoPage</h1>
+        <p>Back to <Link to={"/"}>Home</Link></p>
+    </div>
+  )
+}
+
+export default AddTodoPage
